cd1d22f2bc59bc3589bf515e292be51568792e8d
Travis Purcell <travis.n.purcell@gmail.com>
2021-10-11 21:49:13 -0400

    Replace image links and responsiveness alert

--- themes/portfolio/template-parts/partials/ptl-clients-content.php
+++ themes/portfolio/template-parts/partials/ptl-clients-content.php
@@ -2,7 +2,7 @@
     <div class="container-fluid grid nopadding">
         <div class="row">
             <div class="col-xs-12 clientContent">
-                <a href="https://bordentownhistory.org/" target="_blank"><img class="img-responsive" src="http://portfolio.test/wp-content/uploads/2021/10/BHS_Logo.png" alt="Bordentown Historical Society logo"></a>
+                <a href="https://bordentownhistory.org/" target="_blank"><img class="img-responsive" src="https://tnp-portfolio.com/wp-content/uploads/2021/10/BHS_Logo-1.png" alt="Bordentown Historical Society logo"></a>
             </div>
         </div>
     </div>
