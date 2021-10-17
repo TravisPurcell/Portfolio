80bf197006b48235bd0870fe4aaffd80cac6523f
Travis Purcell <travis.n.purcell@gmail.com>
2021-10-16 20:50:17 -0400

    Move Angular script and remove unnecessary JS

--- themes/portfolio/header.php
+++ themes/portfolio/header.php
@@ -31,7 +31,6 @@
 	<link rel="preconnect" href="https://fonts.googleapis.com">
 	<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin><link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
-	<script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.8.2/angular.min.js"></script>
 
 	<?php wp_head(); ?>
 </head>
